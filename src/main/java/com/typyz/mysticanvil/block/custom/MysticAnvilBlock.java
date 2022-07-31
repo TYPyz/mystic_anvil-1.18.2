f07ff9b14b8426514f511fd9e142b11f42189cdb
TYPyz <marchetti.alessandro2d@gmail.com>
2022-07-31 18:49:28 +0200

    test

--- src/main/java/com/typyz/mysticanvil/block/custom/MysticAnvilBlock.java
+++ src/main/java/com/typyz/mysticanvil/block/custom/MysticAnvilBlock.java
@@ -1,7 +1,8 @@
 package com.typyz.mysticanvil.block.custom;
 
-import com.typyz.mysticanvil.block.custom.VoxelShapeHelper;
 
+
+import com.typyz.mysticanvil.mysticAnvil;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.item.context.BlockPlaceContext;
@@ -20,38 +21,60 @@ import net.minecraft.world.phys.shapes.VoxelShape;
 
 import javax.annotation.Nonnull;
 import java.util.EnumMap;
+import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 
 public class MysticAnvilBlock extends Block {
     public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
 
+
+
+    private static final Map<Direction, VoxelShape> SHAPES = new EnumMap<>(Direction.class);
+
+    private static final Optional<VoxelShape> SHAPE = Stream.of(
+            Block.box(1.125, 0, 0.875, 15.125, 9, 14.875),
+            Block.box(14.125, 9, 0.875, 15.125, 10, 14.875),
+            Block.box(1.125, 9, 0.875, 15.125, 10, 1.875),
+            Block.box(1.125, 9, 13.875, 15.125, 10, 14.875),
+            Block.box(1.125, 9, 0.875, 2.125, 10, 14.875),
+            Block.box(0.875, 15, 6.875, 1.875, 16, 8.875),
+            Block.box(1.875, 15, 6.375, 2.875, 16, 9.375),
+            Block.box(2.125, 14.125, 6.625, 3.125, 15.125, 9.125),
+            Block.box(3.125, 14, 5.875, 4.125, 15, 9.875),
+            Block.box(2.875, 15, 5.625, 3.875, 16, 10.125),
+            Block.box(3.875, 15, 4.525, 12.875, 16, 11.225),
+            Block.box(4.125, 14, 5.125, 12.125, 15, 10.625),
+            Block.box(1.125, 14.25, 7.125, 2.125, 15.25, 8.625),
+            Block.box(3.625, 9, 10.875, 5.625, 10, 12.875),
+            Block.box(10.625, 9, 10.875, 12.625, 10, 12.875),
+            Block.box(10.625, 9, 2.875, 12.625, 10, 4.875),
+            Block.box(3.625, 9, 2.875, 5.625, 10, 4.875),
+            Block.box(11.125, 9.25, 3.375, 12.125, 10.25, 4.375),
+            Block.box(11.125, 9.25, 11.375, 12.125, 10.25, 12.375),
+            Block.box(4.125, 9.25, 11.375, 5.125, 10.25, 12.375),
+            Block.box(4.125, 9.25, 3.375, 5.125, 10.25, 4.375),
+            Block.box(3.125, 9, 4.875, 13.125, 10, 10.875),
+            Block.box(4.125, 10, 5.875, 12.125, 11, 9.875),
+            Block.box(5.125, 11, 6.175, 11.125, 14, 9.575)
+            ).reduce((v1, v2) -> Shapes.join(v1, v2, BooleanOp.OR));
+
     public MysticAnvilBlock(Properties properties) {
         super(properties);
+        registerDefaultState(defaultBlockState().setValue(FACING, Direction.NORTH));
+        runCalculation(SHAPE.orElse(Shapes.block()));
     }
 
-    public static shape Stream.of(Block.box(1, 0, 1, 15, 9, 15),
-            Shapes.join(Block.box(14, 9, 1, 15, 10, 15), Shapes.join(Block.box(1, 9, 1, 2, 10, 15), Shapes.join(Block.box(1, 9, 1, 15, 10, 2), Block.box(1, 9, 14, 15, 10, 15), BooleanOp.OR), BooleanOp.OR), BooleanOp.OR),
-            Shapes.join(Block.box(6.3, 11, 5, 9.7, 14, 11), Shapes.join(Block.box(6, 10, 4, 10, 11, 12), Block.box(5, 9, 3, 11, 10, 13), BooleanOp.OR), BooleanOp.OR),
-            Shapes.join(Block.box(5.25, 14, 4, 10.75, 15, 12), Shapes.join(Block.box(4.65, 15, 3.25, 11.35, 16, 12.25), Shapes.join(Block.box(5.75, 15, 12.25, 10.25, 16, 13.25), Shapes.join(Block.box(6, 14, 12, 10, 15, 13), Shapes.join(Block.box(6.75, 14.125, 13, 9.25, 15.125, 14), Shapes.join(Block.box(6.5, 15, 13.25, 9.5, 16, 14.25), Shapes.join(Block.box(7, 15, 14.25, 9, 16, 15.25), Block.box(7.25, 14.25, 14, 8.75, 15.25, 15), BooleanOp.OR), BooleanOp.OR), BooleanOp.OR), BooleanOp.OR), BooleanOp.OR), BooleanOp.OR), BooleanOp.OR),
-            Shapes.join(Block.box(3, 9, 10.5, 5, 10, 12.5), Shapes.join(Block.box(3, 9, 3.5, 5, 10, 5.5), Shapes.join(Block.box(11, 9, 3.5, 13, 10, 5.5), Block.box(11, 9, 10.5, 13, 10, 12.5), BooleanOp.OR), BooleanOp.OR), BooleanOp.OR),
-            Shapes.join(Block.box(11.5, 9.25, 11, 12.5, 10.25, 12), Shapes.join(Block.box(11.5, 9.25, 4, 12.5, 10.25, 5), Shapes.join(Block.box(3.5, 9.25, 4, 4.5, 10.25, 5), Block.box(3.5, 9.25, 11, 4.5, 10.25, 12), BooleanOp.OR), BooleanOp.OR), BooleanOp.OR)
-            ).reduce((v1, v2) -> Shapes.join(v1, v2, BooleanOp.OR)).get()
+    @Override
+    public VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        return SHAPES.get(state.getValue(FACING));
+    }
 
-     VoxelShapeHelper.rotateShape( , Direction.EAST) ;
-
-//    private static final VoxelShape SHAPE = Stream.of(Block.box(1, 0, 1, 15, 9, 15),
-//            Shapes.join(Block.box(14, 9, 1, 15, 10, 15), Shapes.join(Block.box(1, 9, 1, 2, 10, 15), Shapes.join(Block.box(1, 9, 1, 15, 10, 2), Block.box(1, 9, 14, 15, 10, 15), BooleanOp.OR), BooleanOp.OR), BooleanOp.OR),
-//            Shapes.join(Block.box(6.3, 11, 5, 9.7, 14, 11), Shapes.join(Block.box(6, 10, 4, 10, 11, 12), Block.box(5, 9, 3, 11, 10, 13), BooleanOp.OR), BooleanOp.OR),
-//            Shapes.join(Block.box(5.25, 14, 4, 10.75, 15, 12), Shapes.join(Block.box(4.65, 15, 3.25, 11.35, 16, 12.25), Shapes.join(Block.box(5.75, 15, 12.25, 10.25, 16, 13.25), Shapes.join(Block.box(6, 14, 12, 10, 15, 13), Shapes.join(Block.box(6.75, 14.125, 13, 9.25, 15.125, 14), Shapes.join(Block.box(6.5, 15, 13.25, 9.5, 16, 14.25), Shapes.join(Block.box(7, 15, 14.25, 9, 16, 15.25), Block.box(7.25, 14.25, 14, 8.75, 15.25, 15), BooleanOp.OR), BooleanOp.OR), BooleanOp.OR), BooleanOp.OR), BooleanOp.OR), BooleanOp.OR), BooleanOp.OR),
-//            Shapes.join(Block.box(3, 9, 10.5, 5, 10, 12.5), Shapes.join(Block.box(3, 9, 3.5, 5, 10, 5.5), Shapes.join(Block.box(11, 9, 3.5, 13, 10, 5.5), Block.box(11, 9, 10.5, 13, 10, 12.5), BooleanOp.OR), BooleanOp.OR), BooleanOp.OR),
-//            Shapes.join(Block.box(11.5, 9.25, 11, 12.5, 10.25, 12), Shapes.join(Block.box(11.5, 9.25, 4, 12.5, 10.25, 5), Shapes.join(Block.box(3.5, 9.25, 4, 4.5, 10.25, 5), Block.box(3.5, 9.25, 11, 4.5, 10.25, 12), BooleanOp.OR), BooleanOp.OR), BooleanOp.OR)
-//            ).reduce((v1, v2) -> Shapes.join(v1, v2, BooleanOp.OR)).get();
-
-//    @Override
-//    public VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
-//        return SHAPE;
-//    }
+    protected void runCalculation(VoxelShape shape) {
+        for (Direction direction : Direction.values())
+            SHAPES.put(direction, mysticAnvil.calculateShapes(direction, shape));
+    }
 
     /* FACING */
 
--- src/main/java/com/typyz/mysticanvil/mysticAnvil.java
+++ src/main/java/com/typyz/mysticanvil/mysticAnvil.java
@@ -5,8 +5,11 @@ import com.typyz.mysticanvil.block.ModBlocks;
 import com.typyz.mysticanvil.item.ModItems;
 import net.minecraft.client.renderer.ItemBlockRenderTypes;
 import net.minecraft.client.renderer.RenderType;
+import net.minecraft.core.Direction;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -49,5 +52,20 @@ public class mysticAnvil<eventBus>
             LOGGER.info("HELLO FROM PREINIT");
             LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
         }
+    }
+
+    // ROTATE VOXEL SHAPES
+    public static VoxelShape calculateShapes(Direction to, VoxelShape shape) {
+        final VoxelShape[] buffer = { shape, Shapes.empty() };
+
+        final int times = (to.get2DDataValue() - Direction.NORTH.get2DDataValue() + 4) % 4;
+        for (int i = 0; i < times; i++) {
+            buffer[0].forAllBoxes((minX, minY, minZ, maxX, maxY, maxZ) -> buffer[1] = Shapes.or(buffer[1],
+                    Shapes.create(1 - maxZ, minY, minX, 1 - minZ, maxY, maxX)));
+            buffer[0] = buffer[1];
+            buffer[1] = Shapes.empty();
+        }
+
+        return buffer[0];
     }
 }
